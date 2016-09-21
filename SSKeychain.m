b9c59101488f91f3794998e17f3754f87f1a809c
Don Huff <donh@posim.com>
2016-09-21 08:34:14 -0500

    Rename SSKeychain to RSKeychain to avoid naming conflicts with other libraries.

--- SSKeychain.m
+++ SSKeychain.m
@@ -22,11 +22,11 @@ NSString *const kSSKeychainWhereKey = @"svce";
 CFTypeRef SSKeychainAccessibilityType = NULL;
 #endif
 
-@interface SSKeychain ()
+@interface RSKeychain ()
 + (NSMutableDictionary *)_queryForService:(NSString *)service account:(NSString *)account;
 @end
 
-@implementation SSKeychain
+@implementation RSKeychain
 
 #pragma mark - Getting Accounts
 
